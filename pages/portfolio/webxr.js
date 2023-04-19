012a1c6b05d5719a07d9d289f82c5aa89bf73d08
jy3524 <70925376+jy3524@users.noreply.github.com>
2023-04-19 15:47:19 -0400

    webvr

--- pages/portfolio/webxr.js
+++ pages/portfolio/webxr.js
@@ -9,9 +9,6 @@ const webxr = () => {
       </Title>
       <List>
         <PortfolioImage src="/webxr.png" alt="WebXR"/>
-        <ListItem fontFamily="Gill Sans" fontSize={18} fontWeight="bold" mt={6} mb={6}>
-          A-frame WebVR and Unity VR - WebGL projects are in progress.
-        </ListItem>
         <ListItem fontFamily="Gill Sans" fontSize={24} fontWeight="bold" mt={6} mb={6}>
           Dancer
         </ListItem>
@@ -23,11 +20,11 @@ const webxr = () => {
         </Center>
         <Center>
           <Link fontFamily="Gill Sans" fontSize={20} color="purple.300" href='https://marbled-rambunctious-seahorse.glitch.me/' isExternal>
-            Try AR on your mobile device.
+            Scan the QR Code.
           </Link>
         </Center>
         <ListItem fontFamily="Gill Sans" fontSize={18} mt={6}>
-          Touch screen to make the dancer 2X bigger.
+          Try the AR dancer on your mobile device. Touch screen to make the dancer 2X bigger.
         </ListItem>
         <AspectRatio maxW="580" ratio={1} mt={6}>
           <iframe 
@@ -39,6 +36,28 @@ const webxr = () => {
         </AspectRatio>
         <ListItem fontFamily="Gill Sans" fontSize={18} mt={6}>
           The 3D Character is from Ready Player Me.
+        </ListItem>
+        <ListItem fontFamily="Gill Sans" fontSize={24} fontWeight="bold" mt={12} mb={6}>
+          Interactive Portfolio Website - VR
+        </ListItem>
+        <PortfolioImage src="/webvr.png" alt="WebVR"/>
+        <ListItem fontFamily="Gill Sans" fontSize={18} fontWeight="bold" mt={6} mb={6}>
+          This WebVR project is created using A-frame.
+        </ListItem>
+        <Link fontFamily="Gill Sans" fontSize={20} color="purple.300" href='https://common-wholesale-protest.glitch.me/' isExternal>
+          Try with your HMD device.
+        </Link>
+        <ListItem fontFamily="Gill Sans" fontSize={18} mt={6}>
+          Use your HMD device to enter a fully immersive VR website. 
+          Clicking a link above and then clicking the VR button on the right bottom corner to enter VR mode.
+          Locomotion with the left thumbstick on your left VR controller and turn around with the right thumbstick on your right VR controller.
+          There are VR controller interactable elements in the scene.
+          You can also move around with WASD keys and a mouse but you cannot interact with elements with a keyboard and mouse.
+          This interactive VR website is a prototype landing page for a portfolio website to present 3D models and animations.
+          There is also a routing feature so that I can link other VR or 3D interactive websites with this landing page.
+        </ListItem>
+        <ListItem fontFamily="Gill Sans" fontSize={24} fontWeight="bold" mt={12} mb={6}>
+          Unity VR - WebGL project is in progress.
         </ListItem>
       </List>
     </Container>
